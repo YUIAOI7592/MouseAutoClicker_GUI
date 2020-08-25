83f766e7a60ca5f168535a6027434a3cd89858bf
YUIAOI <qwe153246@gmail.com>
2020-08-25 11:49:56 +0800

    all

---
+++ bin/CMakeFiles/MouseAutoClicker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MouseAutoClicker.dir/main.cpp.obj"
+  "MouseAutoClicker.exe"
+  "MouseAutoClicker.exe.manifest"
+  "MouseAutoClicker.pdb"
+  "libMouseAutoClicker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MouseAutoClicker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
