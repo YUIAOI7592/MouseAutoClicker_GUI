b1ec68b3669603bcadb583df32f70ae09511aac2
YUIAOI <qwe153246@gmail.com>
2020-08-27 07:34:49 +0800

    CBN_SELCHANGE BN_CLICKED結構調整

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #define IDB_START 3301
 #define IDB_PAUSE 3302
 #define IDB_END 3303
+#define IDB_HOTKEY1 3304
+#define IDB_HOTKEY2 3305
 #define GETKEY_TIMER 1
 #include <windows.h>
 #include <windowsx.h>
@@ -9,6 +11,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <thread>
 using namespace std;
 void Clicker();
@@ -20,16 +23,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
                          LPARAM lparam) {
   switch (message) {
     case WM_CREATE: {
-      HWND Shwnd = CreateWindow(L"Button", L"開始",
-                                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 45, 260,
-                                50, 30, hwnd, (HMENU)IDB_START, NULL, NULL);
-      HWND Phwnd = CreateWindow(L"Button", L"暫停",
-                                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 125, 260,
-                                50, 30, hwnd, (HMENU)IDB_PAUSE, NULL, NULL);
-      HWND Ehwnd = CreateWindow(L"Button", L"結束",
-                                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 205, 260,
-                                50, 30, hwnd, (HMENU)IDB_END, NULL, NULL);
-      EnableWindow(GetDlgItem(hwnd, IDB_PAUSE), false);
+      CreateWindow(L"Button", L"開始", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                   45, 260, 50, 30, hwnd, (HMENU)IDB_START, NULL, NULL);
+      CreateWindow(L"Button", L"暫停",
+                   WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | WS_DISABLED, 125,
+                   260, 50, 30, hwnd, (HMENU)IDB_PAUSE, NULL, NULL);
+      CreateWindow(L"Button", L"結束", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                   205, 260, 50, 30, hwnd, (HMENU)IDB_END, NULL, NULL);
+      HWND HotKey1 =
+          CreateWindow(L"COMBOBOX", L"IDB_HOTKEY1",
+                       WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL,
+                       10, 10, 100, 200, hwnd, (HMENU)IDB_HOTKEY1, NULL, NULL);
+      int n = 0;
+      n = ComboBox_AddString(HotKey1, TEXT("Ctrl"));
+      ComboBox_SetItemData(HotKey1, n, VK_CONTROL);
+      n = ComboBox_AddString(HotKey1, TEXT("Alt"));
+      ComboBox_SetItemData(HotKey1, n, VK_MENU);
+      n = ComboBox_AddString(HotKey1, TEXT("Shift"));
+      ComboBox_SetItemData(HotKey1, n, VK_SHIFT);
+      ComboBox_SetCurSel(HotKey1, 0);
+      HWND HotKey2 =
+          CreateWindow(L"COMBOBOX", L"IDB_HOTKEY2",
+                       WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL,
+                       120, 10, 100, 200, hwnd, (HMENU)IDB_HOTKEY2, NULL, NULL);
+
       SetTimer(hwnd, GETKEY_TIMER, 100, NULL);
       return 0;
     }
@@ -43,37 +60,67 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
       return 0;
     }
     case WM_COMMAND: {
-      switch (LOWORD(wparam)) {
-        case IDB_START: {
-          EnableWindow(GetDlgItem(hwnd, IDB_START), false);
-          EnableWindow(GetDlgItem(hwnd, IDB_PAUSE), true);
-          Click = true;
-          if (!ClickFlag) {
-            thread ClickerThread(Clicker);
-            ClickerThread.detach();
+      switch (HIWORD(wparam)) {
+        case CBN_SELCHANGE: {
+          switch (LOWORD(wparam)) {
+            case IDB_HOTKEY1: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_CONTROL: {
+                  MessageBox(hwnd, L"VK_CONTROL", L"CBN_SELCHANGE", MB_OK);
+                  break;
+                }
+                case VK_MENU: {
+                  MessageBox(hwnd, L"VK_MENU", L"CBN_SELCHANGE", MB_OK);
+                  break;
+                }
+                case VK_SHIFT: {
+                  MessageBox(hwnd, L"VK_SHIFT", L"CBN_SELCHANGE", MB_OK);
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+            default:
+              break;
           }
-          ClickFlag = true;
           return 0;
         }
-        case IDB_PAUSE: {
-          EnableWindow(GetDlgItem(hwnd, IDB_PAUSE), false);
-          EnableWindow(GetDlgItem(hwnd, IDB_START), true);
-          Click = false;
-          ClickFlag = false;
-          return 0;
+        case BN_CLICKED: {
+          switch (LOWORD(wparam)) {
+            case IDB_START: {
+              Button_Enable(GetDlgItem(hwnd, IDB_START), false);
+              Button_Enable(GetDlgItem(hwnd, IDB_PAUSE), true);
+              Click = true;
+              if (!ClickFlag) {
+                thread ClickerThread(Clicker);
+                ClickerThread.detach();
+              }
+              ClickFlag = true;
+              break;
+            }
+            case IDB_PAUSE: {
+              Button_Enable(GetDlgItem(hwnd, IDB_PAUSE), false);
+              Button_Enable(GetDlgItem(hwnd, IDB_START), true);
+              Click = false;
+              ClickFlag = false;
+              break;
+            }
+            case IDB_END: {
+              DestroyWindow(hwnd);
+              break;
+            }
+            default:
+              break;
+          }
+          break;
         }
-        case IDB_END: {
-          DestroyWindow(hwnd);
-          return 0;
-        }
-
         default:
-          return 0;
+          break;
       }
-    }
-      return 0;
-    default: {
-      return DefWindowProc(hwnd, message, wparam, lparam);
+      break;
     }
     case WM_TIMER: {
       if (GetAsyncKeyState(VK_CONTROL) & 0x8000 &&
@@ -88,7 +135,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
           GetAsyncKeyState(VK_F3) & 0x8000) {
         SendMessage(hwnd, WM_COMMAND, IDB_END, 0);
       }
+      break;
     }
+    default:
+      return DefWindowProc(hwnd, message, wparam, lparam);
   }
   return 0;
 }
