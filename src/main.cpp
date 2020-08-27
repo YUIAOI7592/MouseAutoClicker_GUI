c2775a271f161440a95a8b636343ef388c326203
YUIAOI <qwe153246@gmail.com>
2020-08-28 00:04:52 +0800

    鐵憨憨行為終於完工啦！雖然不夠簡潔。

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,10 @@
 using namespace std;
 void Clicker();
 void TimerProc(HWND hwnd, UINT nmsg, UINT ntimerid, DWORD dwtime);
-bool Click = false;
-bool ClickFlag = false;
+bool Click = false, ClickFlag = false;
+int HotKeyStart1 = VK_CONTROL, HotKeyPause1 = VK_CONTROL,
+    HotKeyEnd1 = VK_CONTROL;
+int HotKeyStart2 = VK_F1, HotKeyPause2 = VK_F2, HotKeyEnd2 = VK_F3;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
                          LPARAM lparam) {
@@ -46,6 +48,28 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
       n = ComboBox_AddString(HOTKEYSTART1, TEXT("Shift"));
       ComboBox_SetItemData(HOTKEYSTART1, n, VK_SHIFT);
       ComboBox_SetCurSel(HOTKEYSTART1, 0);
+      HWND HOTKEYPAUSE1 = CreateWindow(
+          L"COMBOBOX", L"IDB_HOTKEYPAUSE1",
+          WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL, 140, 40, 60,
+          80, hwnd, (HMENU)IDB_HOTKEYPAUSE1, NULL, NULL);
+      n = ComboBox_AddString(HOTKEYPAUSE1, TEXT("Ctrl"));
+      ComboBox_SetItemData(HOTKEYPAUSE1, n, VK_CONTROL);
+      n = ComboBox_AddString(HOTKEYPAUSE1, TEXT("Alt"));
+      ComboBox_SetItemData(HOTKEYPAUSE1, n, VK_MENU);
+      n = ComboBox_AddString(HOTKEYPAUSE1, TEXT("Shift"));
+      ComboBox_SetItemData(HOTKEYPAUSE1, n, VK_SHIFT);
+      ComboBox_SetCurSel(HOTKEYPAUSE1, 0);
+      HWND HOTKEYEND1 = CreateWindow(
+          L"COMBOBOX", L"IDB_HOTKEYEND1",
+          WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL, 140, 70, 60,
+          80, hwnd, (HMENU)IDB_HOTKEYEND1, NULL, NULL);
+      n = ComboBox_AddString(HOTKEYEND1, TEXT("Ctrl"));
+      ComboBox_SetItemData(HOTKEYEND1, n, VK_CONTROL);
+      n = ComboBox_AddString(HOTKEYEND1, TEXT("Alt"));
+      ComboBox_SetItemData(HOTKEYEND1, n, VK_MENU);
+      n = ComboBox_AddString(HOTKEYEND1, TEXT("Shift"));
+      ComboBox_SetItemData(HOTKEYEND1, n, VK_SHIFT);
+      ComboBox_SetCurSel(HOTKEYEND1, 0);
 
       HWND HOTKEYSTART2 = CreateWindow(
           L"COMBOBOX", L"IDB_HOTKEYSTART2",
@@ -76,6 +100,64 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
       n = ComboBox_AddString(HOTKEYSTART2, TEXT("F12"));
       ComboBox_SetItemData(HOTKEYSTART2, n, VK_F12);
       ComboBox_SetCurSel(HOTKEYSTART2, 0);
+      HWND HOTKEYPAUSE2 = CreateWindow(
+          L"COMBOBOX", L"IDB_HOTKEYPAUSE2",
+          WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL, 230, 40, 60,
+          260, hwnd, (HMENU)IDB_HOTKEYPAUSE2, NULL, NULL);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F1"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F1);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F2"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F2);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F3"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F3);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F4"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F4);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F5"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F5);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F6"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F6);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F7"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F7);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F8"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F8);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F9"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F9);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F10"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F10);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F11"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F11);
+      n = ComboBox_AddString(HOTKEYPAUSE2, TEXT("F12"));
+      ComboBox_SetItemData(HOTKEYPAUSE2, n, VK_F12);
+      ComboBox_SetCurSel(HOTKEYPAUSE2, 1);
+      HWND HOTKEYEND2 = CreateWindow(
+          L"COMBOBOX", L"IDB_HOTKEYEND2",
+          WS_VISIBLE | CBS_DROPDOWNLIST | WS_CHILD | WS_VSCROLL, 230, 70, 60,
+          260, hwnd, (HMENU)IDB_HOTKEYEND2, NULL, NULL);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F1"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F1);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F2"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F2);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F3"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F3);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F4"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F4);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F5"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F5);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F6"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F6);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F7"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F7);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F8"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F8);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F9"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F9);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F10"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F10);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F11"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F11);
+      n = ComboBox_AddString(HOTKEYEND2, TEXT("F12"));
+      ComboBox_SetItemData(HOTKEYEND2, n, VK_F12);
+      ComboBox_SetCurSel(HOTKEYEND2, 2);
 
       SetTimer(hwnd, GETKEY_TIMER, 100, NULL);
       return 0;
@@ -114,15 +196,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
               switch (ComboBox_GetItemData((HWND)lparam,
                                            ComboBox_GetCurSel((HWND)lparam))) {
                 case VK_CONTROL: {
-                  MessageBox(hwnd, L"VK_CONTROL", L"CBN_SELCHANGE", MB_OK);
+                  HotKeyStart1 = VK_CONTROL;
                   break;
                 }
                 case VK_MENU: {
-                  MessageBox(hwnd, L"VK_MENU", L"CBN_SELCHANGE", MB_OK);
+                  HotKeyStart1 = VK_MENU;
                   break;
                 }
                 case VK_SHIFT: {
-                  MessageBox(hwnd, L"VK_SHIFT", L"CBN_SELCHANGE", MB_OK);
+                  HotKeyStart1 = VK_SHIFT;
                   break;
                 }
                 default:
@@ -130,6 +212,215 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
               }
               break;
             }
+            case IDB_HOTKEYPAUSE1: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_CONTROL: {
+                  HotKeyPause1 = VK_CONTROL;
+                  break;
+                }
+                case VK_MENU: {
+                  HotKeyPause1 = VK_MENU;
+                  break;
+                }
+                case VK_SHIFT: {
+                  HotKeyPause1 = VK_SHIFT;
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+            case IDB_HOTKEYEND1: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_CONTROL: {
+                  HotKeyEnd1 = VK_CONTROL;
+                  break;
+                }
+                case VK_MENU: {
+                  HotKeyEnd1 = VK_MENU;
+                  break;
+                }
+                case VK_SHIFT: {
+                  HotKeyEnd1 = VK_SHIFT;
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+            case IDB_HOTKEYSTART2: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_F1: {
+                  HotKeyStart2 = VK_F1;
+                  break;
+                }
+                case VK_F2: {
+                  HotKeyStart2 = VK_F2;
+                  break;
+                }
+                case VK_F3: {
+                  HotKeyStart2 = VK_F3;
+                  break;
+                }
+                case VK_F4: {
+                  HotKeyStart2 = VK_F4;
+                  break;
+                }
+                case VK_F5: {
+                  HotKeyStart2 = VK_F5;
+                  break;
+                }
+                case VK_F6: {
+                  HotKeyStart2 = VK_F6;
+                  break;
+                }
+                case VK_F7: {
+                  HotKeyStart2 = VK_F7;
+                  break;
+                }
+                case VK_F8: {
+                  HotKeyStart2 = VK_F8;
+                  break;
+                }
+                case VK_F9: {
+                  HotKeyStart2 = VK_F9;
+                  break;
+                }
+                case VK_F10: {
+                  HotKeyStart2 = VK_F10;
+                  break;
+                }
+                case VK_F11: {
+                  HotKeyStart2 = VK_F11;
+                  break;
+                }
+                case VK_F12: {
+                  HotKeyStart2 = VK_F12;
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+            case IDB_HOTKEYPAUSE2: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_F1: {
+                  HotKeyPause2 = VK_F1;
+                  break;
+                }
+                case VK_F2: {
+                  HotKeyPause2 = VK_F2;
+                  break;
+                }
+                case VK_F3: {
+                  HotKeyPause2 = VK_F3;
+                  break;
+                }
+                case VK_F4: {
+                  HotKeyPause2 = VK_F4;
+                  break;
+                }
+                case VK_F5: {
+                  HotKeyPause2 = VK_F5;
+                  break;
+                }
+                case VK_F6: {
+                  HotKeyPause2 = VK_F6;
+                  break;
+                }
+                case VK_F7: {
+                  HotKeyPause2 = VK_F7;
+                  break;
+                }
+                case VK_F8: {
+                  HotKeyPause2 = VK_F8;
+                  break;
+                }
+                case VK_F9: {
+                  HotKeyPause2 = VK_F9;
+                  break;
+                }
+                case VK_F10: {
+                  HotKeyPause2 = VK_F10;
+                  break;
+                }
+                case VK_F11: {
+                  HotKeyPause2 = VK_F11;
+                  break;
+                }
+                case VK_F12: {
+                  HotKeyPause2 = VK_F12;
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+            case IDB_HOTKEYEND2: {
+              switch (ComboBox_GetItemData((HWND)lparam,
+                                           ComboBox_GetCurSel((HWND)lparam))) {
+                case VK_F1: {
+                  HotKeyEnd2 = VK_F1;
+                  break;
+                }
+                case VK_F2: {
+                  HotKeyEnd2 = VK_F2;
+                  break;
+                }
+                case VK_F3: {
+                  HotKeyEnd2 = VK_F3;
+                  break;
+                }
+                case VK_F4: {
+                  HotKeyEnd2 = VK_F4;
+                  break;
+                }
+                case VK_F5: {
+                  HotKeyEnd2 = VK_F5;
+                  break;
+                }
+                case VK_F6: {
+                  HotKeyEnd2 = VK_F6;
+                  break;
+                }
+                case VK_F7: {
+                  HotKeyEnd2 = VK_F7;
+                  break;
+                }
+                case VK_F8: {
+                  HotKeyEnd2 = VK_F8;
+                  break;
+                }
+                case VK_F9: {
+                  HotKeyEnd2 = VK_F9;
+                  break;
+                }
+                case VK_F10: {
+                  HotKeyEnd2 = VK_F10;
+                  break;
+                }
+                case VK_F11: {
+                  HotKeyEnd2 = VK_F11;
+                  break;
+                }
+                case VK_F12: {
+                  HotKeyEnd2 = VK_F12;
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
+            }
+
             default:
               break;
           }
@@ -171,16 +462,16 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam,
       break;
     }
     case WM_TIMER: {
-      if (GetAsyncKeyState(VK_CONTROL) & 0x8000 &&
-          GetAsyncKeyState(VK_F1) & 0x8000) {
+      if (GetAsyncKeyState(HotKeyStart1) & 0x8000 &&
+          GetAsyncKeyState(HotKeyStart2) & 0x8000) {
         SendMessage(hwnd, WM_COMMAND, IDB_START, 0);
       }
-      if (GetAsyncKeyState(VK_CONTROL) & 0x8000 &&
-          GetAsyncKeyState(VK_F2) & 0x8000) {
+      if (GetAsyncKeyState(HotKeyPause1) & 0x8000 &&
+          GetAsyncKeyState(HotKeyPause2) & 0x8000) {
         SendMessage(hwnd, WM_COMMAND, IDB_PAUSE, 0);
       }
-      if (GetAsyncKeyState(VK_CONTROL) & 0x8000 &&
-          GetAsyncKeyState(VK_F3) & 0x8000) {
+      if (GetAsyncKeyState(HotKeyEnd1) & 0x8000 &&
+          GetAsyncKeyState(HotKeyEnd2) & 0x8000) {
         SendMessage(hwnd, WM_COMMAND, IDB_END, 0);
       }
       break;
@@ -207,12 +498,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE,
                    CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left,
                    rect.bottom - rect.top, NULL, NULL, NULL, NULL);
+  ShowWindow(window, nCmdShow);
+  UpdateWindow(window);
   MSG msg;
   while (GetMessage(&msg, NULL, 0, 0)) {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
   }
-  return msg.wParam;
+  return (int)msg.wParam;
 }
 void Clicker() {
   enum sendInputs { MOUSE_CLICK, NUM_INPUTS };
